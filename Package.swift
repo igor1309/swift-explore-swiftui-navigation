07d4ad93b1f96f90356764876590faaed37ba655
Igor Malyarov <igor1309@gmail.com>
2022-12-21 11:08:28 +0300

    add Point-Free packages

--- Package.swift
+++ Package.swift
@@ -34,3 +34,74 @@ extension String {
     
     static let shopUIPrototype = "ShopUIPrototype"
 }
+
+// MARK: - Point-Free
+
+private extension Package.Dependency {
+    
+    static let casePaths = Package.Dependency.package(
+        url: .pointFreeGitHub + .case_paths,
+        from: .init(0, 10, 1)
+    )
+    static let combineSchedulers = Package.Dependency.package(
+        url: .pointFreeGitHub + .combine_schedulers,
+        from: .init(0, 9, 1)
+    )
+    static let identifiedCollections = Package.Dependency.package(
+        url: .pointFreeGitHub + .swift_identified_collections,
+        from: .init(0, 4, 1)
+    )
+    static let snapshotTesting = Package.Dependency.package(
+        url: .pointFreeGitHub + .swift_snapshot_testing,
+        from: .init(1, 10, 0)
+    )
+    static let tagged = Package.Dependency.package(
+        url: .pointFreeGitHub + .swift_tagged,
+        from: .init(0, 7, 0)
+    )
+}
+
+private extension Target.Dependency {
+    
+    static let casePaths = product(
+        name: .casePaths,
+        package: .case_paths
+    )
+    static let combineSchedulers = product(
+        name: .combineSchedulers,
+        package: .combine_schedulers
+    )
+    static let identifiedCollections = product(
+        name: .identifiedCollections,
+        package: .swift_identified_collections
+    )
+    static let snapshotTesting = product(
+        name: .snapshotTesting,
+        package: .swift_snapshot_testing
+    )
+    static let tagged = product(
+        name: .tagged,
+        package: .swift_tagged
+    )
+}
+
+private extension String {
+    
+    static let pointFreeGitHub = "https://github.com/pointfreeco/"
+    
+    static let casePaths = "CasePaths"
+    static let case_paths = "swift-case-paths"
+    
+    static let combineSchedulers = "CombineSchedulers"
+    static let combine_schedulers = "combine-schedulers"
+    
+    static let identifiedCollections = "IdentifiedCollections"
+    static let swift_identified_collections = "swift-identified-collections"
+    
+    static let snapshotTesting = "SnapshotTesting"
+    static let swift_snapshot_testing = "swift-snapshot-testing"
+    
+    static let tagged = "Tagged"
+    static let swift_tagged = "swift-tagged"
+}
+
