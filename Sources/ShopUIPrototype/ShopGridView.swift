07d97e3976277e9fc379d972aeafc00f0d821477
Igor Malyarov <igor1309@gmail.com>
2022-12-21 08:26:21 +0300

    cleanup, add target `ShopUIPrototype`

---
+++ Sources/ShopUIPrototype/ShopGridView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopGridView.swift
+//  
+//
+//  Created by Igor Malyarov on 21.12.2022.
+//
+
+import SwiftUI
+
+struct ShopGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopGridView()
+    }
+}
--- Sources/swift-explore-navigation/swift_explore_navigation.swift
+++
@@ -1,6 +0,0 @@
-public struct swift_explore_navigation {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
