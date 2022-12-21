633bd2fc95fb3de61db5ebd54d129178a926fa50
Igor Malyarov <igor1309@gmail.com>
2022-12-21 11:22:55 +0300

    rename package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-explore-navigation",
+    name: "swift-explore-swiftui-navigation",
     platforms: [
         .iOS(.v16)
     ],
