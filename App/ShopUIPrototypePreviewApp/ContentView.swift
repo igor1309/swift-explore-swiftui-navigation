4d417e848a8fb7f1eaab3d6359330a21dcb1ee73
Igor Malyarov <igor1309@gmail.com>
2022-12-21 11:30:58 +0300

    add `ShopUIPrototypePreviewApp`

---
+++ App/ShopUIPrototypePreviewApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ShopUIPrototypePreviewApp
+//
+//  Created by Igor Malyarov on 21.12.2022.
+//
+
+import ShopUIPrototype
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/ShopUIPrototypePreviewApp/ShopUIPrototypePreviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUIPrototypePreviewAppApp.swift
+//  ShopUIPrototypePreviewApp
+//
+//  Created by Igor Malyarov on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopUIPrototypePreviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
