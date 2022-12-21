e511f39b3fed4958b3f9315cc170df65116b98b0
Igor Malyarov <igor1309@gmail.com>
2022-12-21 11:23:27 +0300

    add Xcode project with empty Package.swift

---
+++ App/ExploreSwiftUINavigation/ExploreSwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreSwiftUINavigationApp.swift
+//  ExploreSwiftUINavigation
+//
+//  Created by Igor Malyarov on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExploreSwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
