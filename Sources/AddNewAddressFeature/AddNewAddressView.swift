30f4968cab95d0409fe7fe5fcfd10236fa102200
Igor Malyarov <igor1309@gmail.com>
2022-12-24 00:51:35 +0300

    add `AddNewAddressViewModel` scaffolding

--- Sources/AddNewAddressFeature/AddNewAddressView.swift
+++ Sources/AddNewAddressFeature/AddNewAddressView.swift
@@ -7,14 +7,22 @@
 
 import SwiftUI
 
-struct AddNewAddressView: View {
-    var body: some View {
+public struct AddNewAddressView: View {
+    
+    @ObservedObject private var viewModel: AddNewAddressViewModel
+    
+    public init(viewModel: AddNewAddressViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct AddNewAddressView_Previews: PreviewProvider {
     static var previews: some View {
-        AddNewAddressView()
+        AddNewAddressView(viewModel: .init())
+            .preferredColorScheme(.dark)
     }
 }
---
+++ Sources/AddNewAddressFeature/AddNewAddressViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddNewAddressViewModel.swift
+//  
+//
+//  Created by Igor Malyarov on 24.12.2022.
+//
+
+import Foundation
+
+public final class AddNewAddressViewModel: ObservableObject {
+    
+    public init() {}
+}
