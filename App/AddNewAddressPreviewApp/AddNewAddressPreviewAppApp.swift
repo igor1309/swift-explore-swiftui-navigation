6e45f5404c9ee5d25febe3fef9084f4520196ac4
Igor Malyarov <igor1309@gmail.com>
2022-12-24 12:15:41 +0300

    add `AddNewAddressPreviewApp`

---
+++ App/AddNewAddressPreviewApp/AddNewAddressPreviewAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  AddNewAddressPreviewAppApp.swift
+//  AddNewAddressPreviewApp
+//
+//  Created by Igor Malyarov on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AddNewAddressPreviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ App/AddNewAddressPreviewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddNewAddressPreviewApp
+//
+//  Created by Igor Malyarov on 24.12.2022.
+//
+
+import AddNewAddressFeature
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AddNewAddressViewDemo()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
