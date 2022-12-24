b0c88676f3bf190acc0f21567d1068dcb3482548
Igor Malyarov <igor1309@gmail.com>
2022-12-24 12:18:14 +0300

    simplify helpers

--- App/ShopUIPrototypePreviewApp/ContentView.swift
+++ App/ShopUIPrototypePreviewApp/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     // private let route: AppNavigation.Route? = nil
-    private let route: AppNavigation.Route? = Route.logout()
+    private let route: AppNavigation.Route? = .logout
     
     var body: some View {
         AppView(
@@ -37,40 +37,18 @@ struct ContentView_Previews: PreviewProvider {
 // MARK: - Helpers
 
 /// namespace
-private enum Route {
+private extension AppNavigation.Route {
     
-    static func addressPicker() -> AppNavigation.Route {
-        .sheet(.addressPicker())
-    }
-    static func newAddress() -> AppNavigation.Route {
-        .sheet(.addressPicker(.newAddress))
-    }
-    static func profile() -> AppNavigation.Route {
-        .sheet(.profile(.init(profile: .preview, route: nil, logout: { fatalError("unimplemented")} )))
-    }
-    static func logout() -> AppNavigation.Route {
-        .sheet(.profile(.init(profile: .preview, route: .alert(.logout), logout: { fatalError("unimplemented")} )))
-    }
-    static func editProfile() -> AppNavigation.Route {
-        .sheet(.profile(.init(profile: .preview, route: .navigation(.editProfile), logout: { fatalError("unimplemented")} )))
-    }
-    static func orderHistory() -> AppNavigation.Route {
-        .sheet(.profile(.init(profile: .preview, route: .navigation(.orderHistory), logout: { fatalError("unimplemented")} )))
-    }
-    static func faq() -> AppNavigation.Route {
-        .sheet(.profile(.init(profile: .preview, route: .navigation(.faq), logout: { fatalError("unimplemented")} )))
-    }
-    static func cards() -> AppNavigation.Route {
-        .sheet(.profile(.init(profile: .preview, route: .navigation(.cards), logout: { fatalError("unimplemented")} )))
-    }
-    static func shop() -> AppNavigation.Route {
-        .navigation(.shop(.preview))
-    }
-    static func categoryInShop() -> AppNavigation.Route {
-        .navigation(.shop(.init(shop: .preview, route:
-                .category(.preview))))
-    }
-    static func product() -> AppNavigation.Route {
-        .navigation(.shop(.init(shop: .preview, route: .product(.preview))))
-    }
+    static let addressPicker: Self = .sheet(.addressPicker())
+    static let newAddress: Self = .sheet(.addressPicker(.newAddress))
+    static let profile: Self = .sheet(.profile(.init(profile: .preview, route: nil, logout: { fatalError("unimplemented")} )))
+    static let logout: Self = .sheet(.profile(.init(profile: .preview, route: .alert(.logout), logout: { fatalError("unimplemented")} )))
+    static let editProfile: Self = .sheet(.profile(.init(profile: .preview, route: .navigation(.editProfile), logout: { fatalError("unimplemented")} )))
+    static let orderHistory: Self = .sheet(.profile(.init(profile: .preview, route: .navigation(.orderHistory), logout: { fatalError("unimplemented")} )))
+    static let faq: Self = .sheet(.profile(.init(profile: .preview, route: .navigation(.faq), logout: { fatalError("unimplemented")} )))
+    static let cards: Self = .sheet(.profile(.init(profile: .preview, route: .navigation(.cards), logout: { fatalError("unimplemented")} )))
+    static let shop: Self = .navigation(.shop(.preview))
+    static let categoryInShop: Self = .navigation(.shop(.init(shop: .preview, route:
+            .category(.preview))))
+    static let product: Self = .navigation(.shop(.init(shop: .preview, route: .product(.preview))))
 }
