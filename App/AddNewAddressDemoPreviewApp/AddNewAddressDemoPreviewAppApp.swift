e3cb108a09b4c3386f765baef9cef541b2c1145f
Igor Malyarov <igor1309@gmail.com>
2022-12-26 14:20:53 +0300

    add `AddNewAddressDemoPreviewApp` - demo, not a composition of different features

---
+++ App/AddNewAddressDemoPreviewApp/AddNewAddressDemoPreviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddNewAddressDemoPreviewAppApp.swift
+//  AddNewAddressDemoPreviewApp
+//
+//  Created by Igor Malyarov on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AddNewAddressDemoPreviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/AddNewAddressDemoPreviewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddNewAddressDemoPreviewApp
+//
+//  Created by Igor Malyarov on 26.12.2022.
+//
+
+import AddNewAddressFeature
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AddNewAddressViewDemo()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
