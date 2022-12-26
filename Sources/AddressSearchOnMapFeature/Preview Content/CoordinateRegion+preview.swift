1396100025fd8710c4ecb267873f7d1a4ef96365
Igor Malyarov <igor1309@gmail.com>
2022-12-26 15:30:46 +0300

    reorder preview proper instances

--- Sources/AddressSearchOnMapFeature/Preview Content/CoordinateRegion+preview.swift
+++ Sources/AddressSearchOnMapFeature/Preview Content/CoordinateRegion+preview.swift
@@ -10,27 +10,30 @@ import MapDomain
 #if DEBUG
 public extension CoordinateRegion {
     
-    static let barcelonaNeighborhood: Self = .neighborhood(center: .barcelona)
-    static let barcelonaStreet:       Self = .street(center: .barcelona)
     static let londonCity:            Self = .city(center: .london)
-    static let londonTown:            Self = .town(center: .london)
-    static let londonNeighborhood:    Self = .neighborhood(center: .london)
-    static let londonStreet:          Self = .street(center: .london)
     static let moscowCity:            Self = .city(center: .moscow)
-    static let moscowStreet:          Self = .street(center: .moscow)
+
+    static let barcelonaNeighborhood: Self = .neighborhood(center: .barcelona)
+    static let londonNeighborhood:    Self = .neighborhood(center: .london)
     static let moscowNeighborhood:    Self = .neighborhood(center: .moscow)
+    
+    static let barcelonaStreet:       Self = .street(center: .barcelona)
+    static let londonStreet:          Self = .street(center: .london)
+    static let moscowStreet:          Self = .street(center: .moscow)
+
+    static let londonTown:            Self = .town(center: .london)
     
     static func city(center: LocationCoordinate2D) -> Self {
         .init(center: center, span: .city)
-    }
-    static func town(center: LocationCoordinate2D) -> Self {
-        .init(center: center, span: .town)
     }
     static func neighborhood(center: LocationCoordinate2D) -> Self {
         .init(center: center, span: .neighborhood)
     }
     static func street(center: LocationCoordinate2D) -> Self {
-        .init(center: center, span: .street)
+        .init(center: center, span: .address)
+    }
+    static func town(center: LocationCoordinate2D) -> Self {
+        .init(center: center, span: .town)
     }
 }
 #endif
