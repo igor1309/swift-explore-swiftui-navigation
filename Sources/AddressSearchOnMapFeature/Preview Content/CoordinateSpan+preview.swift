ede0871c1ab4f895ebc825d1cc4d7adaa5f3c21f
Igor Malyarov <igor1309@gmail.com>
2022-12-26 15:30:58 +0300

    formatting

--- Sources/AddressSearchOnMapFeature/Preview Content/CoordinateSpan+preview.swift
+++ Sources/AddressSearchOnMapFeature/Preview Content/CoordinateSpan+preview.swift
@@ -10,12 +10,12 @@ import MapDomain
 #if DEBUG
 extension CoordinateSpan {
     
-    static var regional:     Self = .init(8,    8)
-    static var area:         Self = .init(2,    2)
-    static var city:         Self = .init(0.5,  0.5)
-    static var town:         Self = .init(0.1,  0.1)
-    static var neighborhood: Self = .init(0.01, 0.01)
-    static var street:       Self = .init(0.001, 0.001)
+    static var regional:     Self = .init(8,     8)
+    static var area:         Self = .init(2,     2)
+    static var city:         Self = .init(0.5,   0.5)
+    static var town:         Self = .init(0.1,   0.1)
+    static var neighborhood: Self = .init(0.01,  0.01)
+    static var address:      Self = .init(0.001, 0.001)
     
     private init(
         _ latitudeDelta: LocationDegrees,
