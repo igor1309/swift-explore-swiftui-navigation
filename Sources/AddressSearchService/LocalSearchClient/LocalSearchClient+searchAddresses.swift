6378b84a061a15d01356aa7cca34b8fc1c9fd36e
Igor Malyarov <igor1309@gmail.com>
2022-12-27 14:03:37 +0300

    add `searchAddresses` returning addresses publisher to `LocalSearchClient`

--- Sources/AddressSearchService/LocalSearchClient/LocalSearchClient+searchAddresses.swift
+++ Sources/AddressSearchService/LocalSearchClient/LocalSearchClient+searchAddresses.swift
@@ -5,6 +5,7 @@
 //  Created by Igor Malyarov on 26.12.2022.
 //
 
+import Combine
 import MapDomain
 
 extension LocalSearchClient {
@@ -14,5 +15,19 @@ extension LocalSearchClient {
         region: CoordinateRegion? = nil
     ) async throws -> [Address] {
         try await .init(search(completion, region))
+    }
+    
+    public func searchAddresses(
+        _ completion: LocalSearchCompletion,
+        region: CoordinateRegion? = nil
+    ) -> AnyPublisher<Result<[Address], Error>, Never> {
+        Deferred {
+            Future { promise in
+                Task {
+                    try await searchAddresses(completion, region: region)
+                }
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }
