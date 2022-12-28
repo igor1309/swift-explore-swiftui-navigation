5882a7a91bb5eb551537761b231bca2706847898
Igor Malyarov <igor1309@gmail.com>
2022-12-28 10:29:58 +0300

    add `AddressMapSearchPreviewApp` target

---
+++ App/AddressMapSearchPreviewApp/AddressMapSearchPreviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressMapSearchPreviewAppApp.swift
+//  AddressMapSearchPreviewApp
+//
+//  Created by Igor Malyarov on 28.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AddressMapSearchPreviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
